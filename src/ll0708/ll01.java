f0cbac1d13f8857a1388f0af22ba502b3a1bb1a4
rlarkdals579 <rlarkdals579@naver.com>
2022-07-08 13:34:12 +0900

    0708 practice

---
+++ src/ll0708/ll01.java
@@ -0,0 +1,15 @@
+package ll0708;
+
+public class ll01 {
+
+    public static void main(String[] args) {
+        자동차.달리다();
+    }
+}
+
+class 자동차 {
+    static void 달리다() {
+        System.out.println("자동차가 달립니다.");
+    }
+}
+
