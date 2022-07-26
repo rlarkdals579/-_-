578031b1316763fc662af33e5ba6fcd997fa5eca
rlarkdals579 <rlarkdals579@naver.com>
2022-07-26 10:09:39 +0900

    practice

--- src/ll0725/ll03.java
+++ src/ll0725/ll03.java
@@ -6,13 +6,16 @@ class ll03 {
         a계산기.num1 = 10;
         a계산기.num2 = 20;
 
-        int 결과1 = a계산기.수행((num1, num2) -> num1 + num2) ;
+        int 결과1 = a계산기.수행(
+                (num1, num2) -> num1 + num2);
         System.out.println(결과1); // 30
 
-        int 결과2 = a계산기.수행(((num1, num2) -> num1 - num2));
+        int 결과2 = a계산기.수행(
+                (num1, num2) -> num1 - num2);
         System.out.println(결과2); // -10
 
-        int 결과3 = a계산기.수행((num1, num2) -> num1 * num2);
+        int 결과3 = a계산기.수행(
+                (num1, num2) -> num1 * num2);
         System.out.println(결과3); // 300
     }
 }
